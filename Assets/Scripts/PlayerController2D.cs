61a03499bc519b376d25dc571795f1b939af6c13
reyaz <kwishtian@gmail.com>
2025-11-23 20:24:00 +0800

    Layering, Jumping, Lighting Fix

--- Assets/Scripts/PlayerController2D.cs
+++ Assets/Scripts/PlayerController2D.cs
@@ -1,114 +1,85 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
-
-[RequireComponent(typeof(Rigidbody2D))]
-
 public class PlayerController2D : MonoBehaviour
 {
-
     [Header("Movement Settings")]
     public float moveSpeed = 5f;
     public float jumpForce = 8f;
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+    private float moveInput;
+
+    private Animator anim;
+    public Transform spriteChild;
+    private PlayerSFX sfx;
+
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
-
-    private Rigidbody2D rb;
-    private bool isGrounded;
-    private float moveInput;
-    private bool grounded;
-    private bool wasGrounded;
-
-    private Animator anim;
-    private Rigidbody2D body;
-
-    public Transform spriteChild;
-
-    private PlayerSFX sfx;
-
-    [SerializeField] private Transform spriteTransform;
-
+    private float groundedTimeBuffer = 0f;
+    public float groundedDelay = 0.05f;
 
     private void Awake()
     {
-        body = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sfx = GetComponent<PlayerSFX>();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        wasGrounded = isGrounded;
-        Vector3 scale = spriteChild.localScale;
 
+        // Movement input
         moveInput = Input.GetAxis("Horizontal");
-        //if (moveInput > 0.01f)
-        //    spriteTransform.localRotation = Quaternion.Euler(0, 0, 0);
-        //else if (moveInput < -0.01f)
-        //    spriteTransform.localRotation = Quaternion.Euler(0, 180, 0);
 
+        // Flip sprite
+        Vector3 scale = spriteChild.localScale;
         if (moveInput > 0.01f)
             scale.x = Mathf.Abs(scale.x);
         else if (moveInput < -0.01f)
             scale.x = -Mathf.Abs(scale.x);
 
         spriteChild.localScale = scale;
-        anim.SetFloat("Y Velocity", rb.velocity.y);
-        anim.SetBool("Run", moveInput != 0);
-        anim.SetBool("Grounded", grounded);
 
-        if (Input.GetButtonDown("Jump") && grounded)
+      
+
+        // Jump
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+            isGrounded = false;
             sfx.PlayJump();
-            anim.SetTrigger("Jump");
-            
-            grounded = false;
+            anim.SetBool("isJumping", true);
         }
-        //if(!wasGrounded && isGrounded)
-        //{
-        //    anim.ResetTrigger("Land");
-        //}
 
-        if(isGrounded && Mathf.Abs(moveInput) > 0.01f)
+        // Play footstep sounds
+        if (isGrounded && Mathf.Abs(moveInput) > 0.01f)
         {
             sfx.PlayWalk();
         }
+
+        bool checkGround = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (checkGround)
+        {
+            groundedTimeBuffer = groundedDelay;  // reset timer when touching ground
+        }
+        else
+        {
+            groundedTimeBuffer -= Time.deltaTime;
+        }
+
+        isGrounded = groundedTimeBuffer > 0f;
+
+        anim.SetBool("isJumping", !isGrounded);
+
     }
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
-
-        //Ground Check
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-    }
-
-    private void OnDrawGizmosSelected()
-    {
-        if (groundCheck)
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-
-        if (groundCheck != null)
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Ground")
-        {
-            grounded = true;
-        }
+        anim.SetFloat("X Velocity", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("Y Velocity", rb.velocity.y);
     }
 }
